e7825db8d5323c3c0f8748e4efeba279929e026d
Mitchell Kulak <mitchell.kulak@gmail.com>
2018-09-18 14:58:43 -0400

    first commit

---
+++ hw3-functions.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Hans Dulimarta
+ * TODO: Add your name below this line
+ */
+
+/**
+ * Given the ID of a node {rootId}, find all its descendant elements having
+ * its attribute id set and then change their class to {klazName}.
+ * The function returns the number of such elements found.
+ *
+ * @param rootId    the ID of the "root" element to begin searching
+ * @param klazName  the class to assign to each descendant whose id attrib 
+ *                  is set.
+ * @returns {number}
+ */
+function findElementsWithId(rootId, klazName) {
+    /* complete this function */
+    return 0;
+  }
+  
+  /**
+   * The following function finds all elements with attribute 'data-gv-row' (or
+   * 'data-gv-column') and create a table of the desired dimension as a child of
+   * the element.
+   *
+   * @returns NONE
+   */
+  function createTable() {
+    /* complete this function */
+  }
